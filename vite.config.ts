80d0a49493bcbc9a83303ef761705078949d9203
mrxdev01 <officialdata595@gmail.com>
2026-06-23 13:38:28 +0530

    Update v2 vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
+  // This automatically handles the Vercel preset when deployed, 
+  // but keeps the default Cloudflare setup for Lovable's preview.
+  nitro: process.env.VERCEL ? true : false,
   tanstackStart: {
-    // Redirect TanStack Start's bundled server entry to src/server.ts (our SSR error wrapper).
-    // nitro/vite builds from this
     server: { 
       entry: "server",
-      preset: "vercel" // <-- Add this to override the Cloudflare default
     },
   },
 });
